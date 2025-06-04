46e057fd591eff0da93ba45da2decac9e8dd2ea0
GiulioArantes <giulio.arantes@icloud.com>
2025-06-04 17:24:14 -0300

    LadingPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import Menu from "./components/menu/Menu";
 import Categoria from "./components/categoria/Categoria";
-
+import { LandingPage } from "./pages/landingpage/LanginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/login" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
